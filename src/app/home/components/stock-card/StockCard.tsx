ec8d94e66304a027ef612d938f6f70f2a7a6669a
Andres191200 <marianoandres191200@gmail.com>
2025-04-06 00:18:35 -0300

    changed element positions and order within the stock cards

--- src/app/home/components/stock-card/StockCard.tsx
+++ src/app/home/components/stock-card/StockCard.tsx
@@ -16,55 +16,57 @@ const notifyFavouriteAdded = () => toast('Favourite added!', toastOptions['FAVOU
 const notifyFavouriteDeleted = () => toast('Favourite deleted', toastOptions['FAVOURITE_DELETED']);
 
 
-function checkLineColor(data: TImmutableMockData[]) : string{
-    return data[0].lastsale > data[data.length - 1].lastsale ? '#ff3636' : '#0a0' 
+function checkLineColor(data: TImmutableMockData[]): string {
+    return data[0].lastsale > data[data.length - 1].lastsale ? '#ff3636' : '#0a0'
 }
 
 // CHECKS INITIAL CHARACTER OF AN STOCK NAME JUST TO GET A DIFFERENT CHART DATA
-function checkData(stockName:string):TImmutableMockData[]{
-    if(stockName.at(0)?.toLowerCase() === 'a'){
+function checkData(stockName: string): TImmutableMockData[] {
+    if (stockName.at(0)?.toLowerCase() === 'a') {
         return mockData;
     }
-  return mockData2;
+    return mockData2;
 }
 
-function addFav(stock:IStockCard, addFavourite:(stock:IStockCard) => void) {
+function addFav(stock: IStockCard, addFavourite: (stock: IStockCard) => void) {
     notifyFavouriteAdded();
     addFavourite(stock);
 }
 
-function deleteFav(key:string, deleteFavourite:(favouriteId:string) => void) {
+function deleteFav(key: string, deleteFavourite: (favouriteId: string) => void) {
     notifyFavouriteDeleted();
     deleteFavourite(key);
 }
 
-export default function StockCard({stock}: {stock: IStockData}){
+export default function StockCard({ stock }: { stock: IStockData }) {
     console.log(stock);
-    const {addFavourite, deleteFavourite, favourites} = useStocksStore();
+    const { addFavourite, deleteFavourite, favourites } = useStocksStore();
 
-    const tooltipContainerStyles:CSSProperties = {
+    const tooltipContainerStyles: CSSProperties = {
         backgroundColor: 'var(--dark-tertiary)',
         border: 'none',
         borderRadius: 'var(--border-radius-sm)'
     }
 
-    return(
-        <div className={styles.stockCardComponent} style={{ '--tooltip-content': `"${stock.name}"`} as React.CSSProperties}>
+    return (
+        <div className={styles.stockCardComponent} style={{ '--tooltip-content': `"${stock.name}"` } as React.CSSProperties}>
             <Toaster />
             <div className={styles.stockCardHeader}>
-                <div className={styles.stockNameContainer}>
-                    <span className={styles.stockName}>{stock.name}</span>
+                <div className={styles.stockSymbolContainer}>
+                    <h5 className={styles.stockSymbol}>{stock.symbol}</h5>
                 </div>
                 <div className={styles.aditionalStockInfoContainer}>
-                    <span>{stock.symbol}</span>
                     <div className={styles.favouriteContainer}>
                         {
-                            favourites.has(stock.symbol) 
-                            ? <Image src={'/favourite-filled.svg'} alt='favourite button' height={25} width={25} onClick={() => deleteFav(stock.symbol, deleteFavourite)}/>
-                            : <Image src={'/favourite.svg'} alt='favourite button' height={25} width={25} onClick={() => addFav(stock, addFavourite)}/>
+                            favourites.has(stock.symbol)
+                                ? <Image src={'/favourite-filled.svg'} alt='favourite button' height={25} width={25} onClick={() => deleteFav(stock.symbol, deleteFavourite)} />
+                                : <Image src={'/favourite.svg'} alt='favourite button' height={25} width={25} onClick={() => addFav(stock, addFavourite)} />
                         }
                     </div>
                 </div>
+            </div>
+            <div className={styles.stockNameContainer}>
+                <span className={styles.stockName}>{stock.name}</span>
             </div>
             <div className={styles.stockValueContainer}>
                 <h4 className={styles.stockValue}>
@@ -72,18 +74,18 @@ export default function StockCard({stock}: {stock: IStockData}){
                 </h4>
             </div>
             <div className={styles.valueHistoryChart}>
-                <LineChart width={420} height={160} data={checkData(stock.name)} margin={{top: 10, bottom: 10}}>
-                    <CartesianGrid stroke='null'/>
-                    <XAxis dataKey="name" fontSize={12}/>
-                    <YAxis 
+                <LineChart width={420} height={160} data={checkData(stock.name)} margin={{ top: 10, bottom: 10 }}>
+                    <CartesianGrid stroke='null' />
+                    <XAxis dataKey="name" fontSize={12} />
+                    <YAxis
                         orientation='right'
                         dataKey="value"
-                        fontSize={12} 
+                        fontSize={12}
                         width={50}
                         domain={[CalculateMinValueInArray(checkData(stock.name)), CalculateMaxValueInArray(checkData(stock.name))]}
                     />
-                    <Tooltip contentStyle={tooltipContainerStyles} labelStyle={{fontSize: '14px'}} itemStyle={{fontSize: '14px'}} />
-                    <Line type="monotone" dataKey="lastsale" stroke={checkLineColor(checkData(stock.name))}/>
+                    <Tooltip contentStyle={tooltipContainerStyles} labelStyle={{ fontSize: '14px' }} itemStyle={{ fontSize: '14px' }} />
+                    <Line type="monotone" dataKey="lastsale" stroke={checkLineColor(checkData(stock.name))} />
                 </LineChart>
             </div>
         </div>
