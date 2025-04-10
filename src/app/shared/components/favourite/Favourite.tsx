e2217bf635ae6763715242ca8e6ab12e7e08acb8
Andres191200 <marianoandres191200@gmail.com>
2025-04-09 21:59:28 -0300

    delete favourite works!

--- src/app/shared/components/favourite/Favourite.tsx
+++ src/app/shared/components/favourite/Favourite.tsx
@@ -1,8 +1,12 @@
 import { TImmutableStockCard } from '@/app/home/components/stock-card/types/stockCard';
 import styles from './styles.module.scss';
 import Image from 'next/image';
+import { useStore } from 'zustand';
+import { useStocksStore } from '@/app/home/store/store';
 
 export default function Favourite({favourite, favKey}: {favourite: TImmutableStockCard, favKey:string}){
+    
+    const {deleteFavourite} = useStocksStore();
     return(
         <div className={styles.favouriteComponent}>
             <div className={styles.favouriteContainer}>
@@ -10,7 +14,9 @@ export default function Favourite({favourite, favKey}: {favourite: TImmutableSto
                 <span>{favKey}</span>
             </div>
             <div className={styles.deleteFavouriteContainer}>
-                <Image src={'/trash.svg'} alt='delete fav' height={25} width={25} />
+                <button type="button" onClick={() => deleteFavourite(favKey)}>
+                    <Image src={'/trash.svg'} alt='delete fav' height={25} width={25} />
+                </button>
             </div>
         </div>
     )
