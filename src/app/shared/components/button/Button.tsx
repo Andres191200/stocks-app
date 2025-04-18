0ff7634e8e79bf2590a6a807cd5d6a45ece61194
Andres191200 <marianoandres191200@gmail.com>
2025-04-17 23:28:33 -0300

    replacing the current html buttons with button custom components. Discriminated Union typescript pattern adapted

--- src/app/shared/components/button/Button.tsx
+++ src/app/shared/components/button/Button.tsx
@@ -1,50 +1,56 @@
+import { SORT_TYPES } from "../../utils/filter-types";
 import styles from "./styles.module.scss";
 
-type ButtonProps = ( ILinkButton & {kind:'link'} | IBaseButton & {kind:'default'})
+type ButtonProps = ( ILinkButton & {kind:'link'} | IBaseButton & {kind:'default'} | IToggleButton & {kind: 'toggle'})
 
-const ButtonKind = {
-  link: LinkButton,
-  toggle: ToggleButton,
-  default: DefaultButton,
-} as const;
-
-interface IBaseButton extends React.HTMLAttributes<HTMLButtonElement> {
+interface IBaseButton extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   text: string;
 }
 
-interface ILinkButton extends React.HTMLAttributes<HTMLButtonElement> {
+interface ILinkButton extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   text: string;
   href: string;
   anchorProps?: React.AnchorHTMLAttributes<HTMLAnchorElement>
 }
 
-function ToggleButton({ onClick, text, ...props }: IBaseButton) {
-    
+interface IToggleButton extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  text: string;
+  active: boolean;
 }
 
-function DefaultButton({ onClick, text, ...props }: IBaseButton) {
+function LinkButton({ href, text, anchorProps, ...props }: ILinkButton) {
   return (
-    <button onClick={onClick} {...props}>
-      {text}
+    <button className={styles.buttonComponent} {...props}>
+      <a href={href} {...anchorProps}>{text}</a>
     </button>
   );
 }
 
-function LinkButton({ href, text, anchorProps, ...props }: ILinkButton) {
-  // WRAP IN THE BUTTON COMPONENT
+function ToggleButton({ onClick, text, active, ...props }: IToggleButton) {
+    return(
+      <button className={`${styles.buttonComponent} ${active ? styles.active : ''}`} onClick={onClick} {...props}>
+        {text}
+      </button>
+    )
+}
+
+function DefaultButton({ onClick, text, ...props }: IBaseButton) {
   return (
-    <button {...props}>
-      <a href={href} {...anchorProps}>{text}</a>
+    <button className={styles.buttonComponent} onClick={onClick} {...props}>
+      {text}
     </button>
   );
 }
 
 export default function Button(props: ButtonProps) {
     const {kind} = props;
+
     switch(kind){
         case 'link':
             return <LinkButton {...props} text={props.text} href={props.href} anchorProps={props.anchorProps}/>
         case 'default':
             return <DefaultButton {...props} text={props.text} onClick={props.onClick} />
+        case 'toggle':
+            return <ToggleButton {...props} text={props.text} onClick={props.onClick} active={props.active}/>
     }
 }
