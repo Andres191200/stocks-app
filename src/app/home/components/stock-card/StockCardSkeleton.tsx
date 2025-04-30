f8824af31fd7d881bbcc92a27042e1979d22acf3
Andrés Vandoni <76135539+Andres191200@users.noreply.github.com>
2025-04-29 21:29:46 -0300

    changed the pulsing animation from the skeleton cards to skeleton inside elements (#34)

--- src/app/home/components/stock-card/StockCardSkeleton.tsx
+++ src/app/home/components/stock-card/StockCardSkeleton.tsx
@@ -5,34 +5,36 @@ import gsap from "gsap";
 export default function StockCardSkeleton() {
   const cardRef = useRef<HTMLDivElement>(null);
   useEffect(() => {
+    const targetElements =
+      cardRef.current?.querySelectorAll(".pulsing-animation");
     const ctx = gsap.context(() => {
-        var tl = gsap.timeline({ repeat: 2, repeatDelay: 3 });
+      targetElements?.forEach((element) => {
+        gsap.to(element, {
+          opacity: 0,
+          repeat: -1,
+          duration: 1,
+          yoyo: true,
+          ease: "power1.inOut",
+        });
+      });
+    }, cardRef);
 
-        gsap.to(cardRef.current, {
-            opacity: 0,
-            repeat: -1,
-            duration: 1,
-            yoyo: true,
-            ease: "power1.inOut"
-        })
-    }, cardRef)
-    
     return () => ctx.revert();
   }, []);
   return (
     <div className={styles.stockCardSkeletonComponent} ref={cardRef}>
       <div className={styles.stockCardHeader}>
         <div className={styles.stockSymbolContainer}>
-          <h5 className={styles.stockSymbol} />
+          <h5 className={`${styles.stockSymbol} pulsing-animation`} />
         </div>
       </div>
       <div className={styles.stockNameContainer}>
-        <span className={styles.stockName} />
+        <span className={`${styles.stockName} pulsing-animation`} />
       </div>
       <div className={styles.stockValueContainer}>
-        <h4 className={styles.stockValue} />
+        <h4 className={`${styles.stockValue} pulsing-animation`} />
       </div>
-      <div className={styles.valueHistoryChart}></div>
+      <div className={`${styles.valueHistoryChart} pulsing-animation`} />
     </div>
   );
 }
