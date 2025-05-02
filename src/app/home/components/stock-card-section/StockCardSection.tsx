db28584ce54a612aa7df92cb1cb75fe8213a403b
Andrés Vandoni <76135539+Andres191200@users.noreply.github.com>
2025-05-01 23:47:23 -0300

    Improvements v3 (#35)
    * changed the pulsing animation from the skeleton cards to skeleton inside elements
    
    * improvements v3: paginator not showing up when filtered items are fewer than the items per page. using searchbar now deactivates/detoggles the sorting buttons, and the other way around (using the sort buttons cleans up the search bar)

--- src/app/home/components/stock-card-section/StockCardSection.tsx
+++ src/app/home/components/stock-card-section/StockCardSection.tsx
@@ -17,8 +17,10 @@ function formatPrice(price:String){
 }
 
 export default function StockCardSection() {
+  const ITEMS_PER_PAGE = 25;
   const params = useSearchParams();
   const currentPage = Number(params.get(FILTER_TYPES.PAGE || 1));
+  
   const {
     isPending,
     error: stocksError,
@@ -57,6 +59,7 @@ export default function StockCardSection() {
     const _error = stocksError as unknown as IError;
     return <Error code={_error.code} errorMessage={_error.errorMessage} />;
   }
+
   return (
     <div className={styles.stockCardSectionComponent}>
       <Filters />
@@ -73,8 +76,9 @@ export default function StockCardSection() {
           )}
         </StockCardGrid>
       )}
-
-      {!isPending ? (
+      {/* PAGINATOR NOT WORKING WITH FILTERED ITEMS (THERE IS NO ENDPOINT IN THE API FOR FILTERING) */}
+      {/* TODO: SAVE THE FILTERED STOCKS SO THE APP DOESN'T CALL THIS FILTERING CALCULATION FUNCTION MULTIPLE TIMES */}
+      {!isPending && formatStocks()!.length >= ITEMS_PER_PAGE ? (
         <Paginator currentPage={currentPage} pagesQuantity={5} />
       ) : null}
     </div>
