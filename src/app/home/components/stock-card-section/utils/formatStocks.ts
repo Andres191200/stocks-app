dda4dc86921f8ead3a23e5ec629e9e633b7dac40
Andres191200 <marianoandres191200@gmail.com>
2025-05-02 23:06:16 -0300

    animations now are in a separated folder depending the component attached to

---
+++ src/app/home/components/stock-card-section/utils/formatStocks.ts
@@ -0,0 +1,33 @@
+import { IStockData } from "@/app/home/actions/getStocksData";
+import { FILTER_TYPES, SORT_TYPES } from "@/app/shared/utils/filter-types";
+import { useSearchParams } from "next/navigation";
+
+function formatPrice(price:String){
+    return Number(price.substring(1));
+  }
+
+export function formatStocks(stocks: IStockData[]): IStockData[] | undefined {
+    const params = useSearchParams();
+    const query = params.get(FILTER_TYPES.SEARCH);
+    const order = params.get(FILTER_TYPES.ORDER);
+    if (query?.trim().length! > 0) {
+      return stocks?.filter((stock) =>
+        stock.name.toLowerCase().includes(query!.toLowerCase())
+      );
+    }
+    if (order) {
+      if (order === SORT_TYPES.ASC) {
+        return stocks?.sort((a, b) => a.name.localeCompare(b.name));
+      }
+      if (order === SORT_TYPES.DESC) {
+        return stocks?.sort((a, b) => b.name.localeCompare(a.name));
+      }
+      if (order === SORT_TYPES.HIGHER) {
+        return stocks?.sort((a, b) => formatPrice(a.lastsale.toString()) < formatPrice(b.lastsale.toString()) ? 1 : -1);
+      }
+      if (order == SORT_TYPES.LOWER) {
+        return stocks?.sort((a, b) => formatPrice(a.lastsale.toString()) > formatPrice(b.lastsale.toString()) ? 1 : -1);
+      }
+    }
+    return stocks;
+  }
--- src/app/home/components/stock-card/StockCard.tsx
+++ src/app/home/components/stock-card/StockCard.tsx
@@ -20,6 +20,7 @@ import { mockData, mockData2 } from "./utils/mockData";
 import toast, { Toaster } from "react-hot-toast";
 import toastOptions from "@/app/shared/utils/toastOptons";
 import gsap from "gsap";
+import { fadeInStockCardFrom, fadeInStockCardTo } from "./utils/animations";
 
 const notifyFavouriteAdded = () =>
   toast("Favourite added!", toastOptions["FAVOURITE_ADDED"]);
@@ -65,8 +66,8 @@ export default function StockCard({ stock, idx }: { stock: IStockData, idx:numbe
     const ctx = gsap.context(() => {
       gsap.fromTo(
         cardRef.current,
-        { opacity: 0, y: 50 },
-        { opacity: 1, y: 0, duration: 1, ease: "power4.out", delay: idx/delayFactor }
+        fadeInStockCardFrom(),
+        fadeInStockCardTo(idx/delayFactor)
       );
     }, cardRef);
 
