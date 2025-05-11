4638ad98767c231de3bb7906f1e4829fb5bbd9bd
Andres191200 <marianoandres191200@gmail.com>
2025-05-11 16:24:59 -0300

    added debounce to toggle theme button to prevent multiple clicks in short time periods

--- src/app/shared/components/toggle-theme/ToggleTheme.tsx
+++ src/app/shared/components/toggle-theme/ToggleTheme.tsx
@@ -1,13 +1,27 @@
-import React from "react";
+import React, { use, useEffect, useRef, useState } from "react";
 import styles from './styles.module.scss';
-import { useStore } from "zustand";
 import { useStocksStore } from "@/app/home/store/store";
 
 export default function ToggleTheme() {
     const {toggleTheme, theme} = useStocksStore();
+    const [disabled, setDisabled] = useState<boolean>(false);
+    const debounceTimerRef = useRef<NodeJS.Timeout>(setTimeout(() => {}));
+
+    function onToggleTheme() {
+      setDisabled(true);
+      toggleTheme();
+      debounceTimerRef.current = setTimeout(() => {
+        setDisabled(false);
+      }, 1000);
+    }
+
+    useEffect(() => {
+      return () => clearTimeout(debounceTimerRef.current);
+    }, [])
+
   return (
     <div className={styles.toggleThemeComponent}>
-      <button onClick={toggleTheme}>
+      <button onClick={() => onToggleTheme()} disabled={disabled}>
         Current theme: {theme.toString()}
       </button>
     </div>
