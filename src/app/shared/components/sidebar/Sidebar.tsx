6c8c5e29f0742d04e22dd57fefe05d7430b8cc46
Andrés Vandoni <76135539+Andres191200@users.noreply.github.com>
2025-05-15 23:01:56 -0300

    sidebar formatted code (#41)

--- src/app/shared/components/sidebar/Sidebar.tsx
+++ src/app/shared/components/sidebar/Sidebar.tsx
@@ -1,28 +1,33 @@
-'use client';
-import { useStocksStore } from '@/app/home/store/store';
-import styles from './styles.module.scss';
-import Favourite from '../favourite/Favourite';
-import Image from 'next/image';
-import ToggleTheme from '../toggle-theme/ToggleTheme';
+"use client";
+import { useStocksStore } from "@/app/home/store/store";
+import styles from "./styles.module.scss";
+import Favourite from "../favourite/Favourite";
+import Image from "next/image";
+import ToggleTheme from "../toggle-theme/ToggleTheme";
 
-export default function Sidebar(){
-    const {favourites} = useStocksStore();
-    return(
-        <div className={`${styles.sidebarComponent} sidebar`}>
-            <h4>My stocks</h4>
-            {/* <ToggleTheme /> */}
-            <div className={styles.favouritesContainer}>
-            {
-                favourites.size !== 0
-                ? [...favourites].map(([key, favourite]) => <Favourite favourite={favourite} favKey={key} key={key}/>)
-                : (
-                    <div className={styles.emptyFavouritesListContainer}>
-                      <Image src={'/warning.svg'} alt="warning icon" height={25} width={25} />
-                      <p>No favourites added</p>
-                    </div>
-                )
-            }
-            </div>
-        </div>
-    )
+export default function Sidebar() {
+  const { favourites } = useStocksStore();
+  return (
+    <div className={`${styles.sidebarComponent} sidebar`}>
+      <h4>My stocks</h4>
+      {/* <ToggleTheme /> */}
+      <div className={styles.favouritesContainer}>
+        {favourites.size !== 0 ? (
+          [...favourites].map(([key, favourite]) => (
+            <Favourite favourite={favourite} favKey={key} key={key} />
+          ))
+        ) : (
+          <div className={styles.emptyFavouritesListContainer}>
+            <Image
+              src={"/warning.svg"}
+              alt="warning icon"
+              height={25}
+              width={25}
+            />
+            <p>No favourites added</p>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 }
